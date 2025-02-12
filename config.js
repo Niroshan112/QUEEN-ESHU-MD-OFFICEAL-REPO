8ea84bc8402de10b5b867f6c59728c7c4c6545b3
Niroshan112 <121544895+Niroshan112@users.noreply.github.com>
2025-02-13 02:55:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,27 +5,27 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ESHU-MD=your_paircode",
-MODE: process.env.MODE || "public",  // Add Your Bot Mode (groups/privet/public)
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=rnYWhDyY#AFP7He2n8YH_QZw1Gil2cTrWWrzLs56IRTmPqlMqjWc",
+MODE: process.env.MODE || "privet",  // Add Your Bot Mode (groups/privet/public)
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
 OWNER_REACT: process.env.OWNER_REACT || "true", // true or false
-BOT_NAME: process.env.BOT_NAME || "ESHU 𝙼𝙳 V1",
-AUTO_VOICE: process.env.AUTO_VOICE || "true", // true or false
-AUTO_STICKER: process.env.AUTO_STICKER || "true", // true or false
+BOT_NAME: process.env.BOT_NAME || "𝙲𝚁𝙰𝚆𝙻 𝙼𝙳",
+AUTO_VOICE: process.env.AUTO_VOICE || "false", // true or false
+AUTO_STICKER: process.env.AUTO_STICKER || "false", // true or false
 AUTO_REPLY: process.env.AUTO_REPLY || "true", // true or false
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true", // true or false
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true", // true or false
-ANTI_LINK: process.env.ANTI_LINK || "true", // true or false
-ANTI_BAD: process.env.ANTI_BAD || "true",// true or false
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true", // true or false
-AUTO_REACT: process.env.AUTO_REACT || "true", // true or false
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false", // true or false
+ANTI_LINK: process.env.ANTI_LINK || "false", // true or false
+ANTI_BAD: process.env.ANTI_BAD || "false",// true or false
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false", // true or false
+AUTO_REACT: process.env.AUTO_REACT || "false", // true or false
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true or false
-FILENAME: process.env.FILENAME || "QUEEN-ESHU-MD-V1",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/xmhW7DJ/6252.png", 
-CHANNEL_NAME: process.env.CHANNEL_NAME || "💗 QUEEN-ESHU-MD-V1 💗",
+FILENAME: process.env.FILENAME || "𝙲𝚁𝙰𝚆𝙻 𝙼𝙳 𝙱𝙾𝚃",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/53sJy4w/2831.jpg", 
+CHANNEL_NAME: process.env.CHANNEL_NAME || "💀 𝙲𝚁𝙰𝚆𝙻 𝙼𝙳 𝙱𝙾𝚃 💀",
 NEWSLETTER_ID: process.env.NEWSLETTER_ID || "120363303242803015@newsletter",
-TITLE: process.env.TITLE || "QUEEN-ESHU-MD-V1",
-BODY: process.env.BODY || "CREATED BY ESHAN TECH SL🛡️",
-WEBURL: process.env.WEBURL|| "https://github.com/CYBER-ESHU/ESHU-MD",
-BOT_IMG: process.env.BOT_IMG || "https://i.ibb.co/xmhW7DJ/6252.png"     
+TITLE: process.env.TITLE || "𝙲𝚁𝙰𝚆𝙻 𝙼𝙳 𝙱𝙾𝚃",
+BODY: process.env.BODY || "ℂℝ𝔼𝔸𝕋𝔼𝔻 𝔹𝕐 ℂℝ𝔸𝕎𝕃 📍",
+WEBURL: process.env.WEBURL|| "https://github.com/Niroshan112",
+BOT_IMG: process.env.BOT_IMG || "https://i.ibb.co/53sJy4w/2831.jpg"     
 };
